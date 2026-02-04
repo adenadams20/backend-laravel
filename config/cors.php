7c93775061a495106ed7d1cac0e3be2b9a7042d6
adenadams20 <adamsdiene@gmail.com>
2026-02-04 09:45:43 +0000

    Fix: Update-2

--- config/cors.php
+++ config/cors.php
@@ -1,27 +1,16 @@
 <?php
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://red-product-cn78.vercel.app',
     ],
-
-    // 🔥 IMPORTANT
     'allowed_headers' => [
-        'Content-Type',
         'Authorization',
-        'X-Requested-With',
+        'Content-Type',
         'Accept',
         'Origin',
+        'X-Requested-With',
     ],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    // ✅ tokens Bearer → FALSE
     'supports_credentials' => false,
 ];
